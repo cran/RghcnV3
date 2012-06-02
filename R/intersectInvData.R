d2d3dfadbddf5acd9e6c8d05d797d8d8b4b085d2
Steven Mosher <moshersteven@gmail.com>
2012-06-02 00:00:00 +0000

    version 2.9

--- R/intersectInvData.R
+++ R/intersectInvData.R
@@ -4,7 +4,10 @@ intersectInvData <- function(inventory, Data){
   if (isMts(Data) | is.zoo(Data) | isArray(Data)){
       Data         <- removeNaStations(Data)
       invStations  <- getStations(inventory)
+      invStations  <- as.character(invStations)
       dataStations <- getStations(Data)
+      
+       
       if(sum(duplicated(dataStations)) != 0)stop("stations in data are duplicated")
       if(sum(duplicated(invStations)) != 0)stop("stations in inventory are duplicated")
       
@@ -12,28 +15,31 @@ intersectInvData <- function(inventory, Data){
        
       if (length(common) == 0) stop("No Stations in intersection")
       inventory    <- inventory[which(invStations %in% common), ]
-      inventory    <- inventory[order(inventory$Id),]
+      inventory    <- inventory[order(as.character(inventory$Id)),]
       if (isMts(Data)){
           Data  <- Data[ ,which(dataStations %in% common)]
           Data  <- Data[ ,order(colnames(Data))]
           if(!identical(getStations(Data) ,getStations(inventory)))stop("mismatched Stations")
-          if(!validData)stop("Invalid Data")
+          if(!validData(Data))stop("Invalid Data")
           return(list(Inventory = inventory, Mts = Data))
       }
       if (is.zoo(Data)){
           Data  <- Data[ ,which(dataStations %in% common)]
           Data  <- Data[ , order(colnames(Data))]
           if(!identical(getStations(Data) ,getStations(inventory)))stop("mismatched Stations")
-          if(!validData)stop("Invalid Data")
+          if(!validData(Data))stop("Invalid Data")
           return(list(Inventory = inventory, Zoo = Data))
         
       }
       if (isArray(Data)){
         
            Data  <- Data[which(dataStations %in% common), , ]
+            
            Data  <- Data[order(unlist(dimnames(Data)[1])),,]
+           
+           
            if(!identical(getStations(Data) ,getStations(inventory)))stop("mismatched Stations")
-           if(!validData)stop("Invalid Data")
+           if(!validData(Data))stop("Invalid Data")
            return(list(Inventory = inventory, Array = Data))
         
       }
